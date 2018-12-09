2f922fe7960f582ac8c5cfb86d855e982923cad7
Connor Gordon <con.gordon@gmail.com>
2018-12-08 22:55:46 -0800

    added notes to make reflect on

--- main.js
+++ main.js
@@ -1,24 +1,32 @@
 
 
-
+//let's set the variable "elements" to select the id templateDiv in html by looking in the document with query selector
+//                                  (\has to be id btw/)
 
 let elements = document.querySelector('#templateDiv')
 
-// I need to get a result from results, and for each of them get the title
+// Loop through items.js and for each loop pull a single result from the array results, "result" essentially means items.results now
 
 items.results.forEach(function(result){
+
+	// let the variable "searchTemplate" equal our html template and for each ${}, get the corresponding value and fill it in
+	// still dont know how to fix the url but w/o that issue the img would be ${result.Images[14]} or $(result.Images["url_570xN"])
+	
 	let	searchTemplate = 
 	`<div class="templateDiv">
 		<ul class="templateList">
-			<li><h5 class="templateimg"> ${result.Images[unescape(14)]}</h5></li>
-			<li><h5 class="templateTitle"> ${result.title}</h5></li>
-			<li><h5 class="templateSeller"> ${result.Shop.shop_name}</h5></li>
-			<li><h5 class="templatePrice"> ${result.price}</h5></li>
+			<li><p class="templateimg"></p></li>
+			<li><p class="templateTitle"> ${result.title}</p></li>
+			<li><p class="templateSeller"> ${result.Shop.shop_name}</p></li>
+			<li><p class="templatePrice"> ${result.price}</p></li>
 		</ul>		
 	</div>`
+
+// use the variable elements we set earlier and innerHTML to add the searchTemplate in place of the HTML
 
 elements.innerHTML += searchTemplate
 })
 
 
 
+// unescape ??? ${result.Images[unescape(14)]} or $(result.Images[14 or "url_570xN"])
